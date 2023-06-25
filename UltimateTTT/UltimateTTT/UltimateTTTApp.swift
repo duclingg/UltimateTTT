c2112c1f8b7e1c0089387eb7cd19dac05f03c4ad
Justin Hoang <58650295+duclingg@users.noreply.github.com>
2023-06-25 15:41:25 -0600

    Add files via upload

---
+++ UltimateTTT/UltimateTTT/UltimateTTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateTTTApp.swift
+//  UltimateTTT
+//
+//  Created by Justin Hoang on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct UltimateTTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
