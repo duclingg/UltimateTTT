78109592c5a46efd8ca8510e68c9c444a4cc0af6
Justin Hoang <justin.lhoang3@gmail.com>
2023-06-25 23:49:25 -0600

    changed game mode selected to buttons

--- UltimateTTT/UltimateTTT/Scenes/Game/Views/GameSetupView.swift
+++ UltimateTTT/UltimateTTT/Scenes/Game/Views/GameSetupView.swift
@@ -10,27 +10,53 @@ import SwiftUI
 struct GameSetupView: View {
     @State private var AISelected = false
     
+    let startColor = Color(red: 0.96, green: 0.18, blue: 0.18)
+    
     var body: some View {
-        VStack {
-            Text("Choose Opponent")
-                .font(.title)
-                .padding()
+        ZStack {
+            Color("backgroundColor")
+                .edgesIgnoringSafeArea(.all)
             
-            Toggle("Play aginst AI", isOn: $AISelected)
-                .font(.title2)
-                .padding()
-            
-            NavigationLink(destination: GameView(AISelected: AISelected)) {
-                Text("Start Game")
+            VStack {
+                Text("Choose Opponent")
                     .font(.title)
-                    .padding()
-                    .background(Color.blue)
                     .foregroundColor(Color("textColor"))
-                    .cornerRadius(10)
+                    .padding()
+                
+                Button {
+                    AISelected = true
+                } label: {
+                    Text("CPU")
+                        .font(.title2)
+                        .padding()
+                        .foregroundColor(AISelected ? .white : Color("textColor"))
+                        .background(AISelected ? Color("buttonColor") : .white)
+                        .cornerRadius(10)
+                }.padding()
+                
+                Button {
+                    AISelected = false
+                } label: {
+                    Text("Pass and Play")
+                        .font(.title2)
+                        .padding()
+                        .foregroundColor(AISelected ? Color("textColor") : .white)
+                        .background(AISelected ? .white : Color("buttonColor"))
+                        .cornerRadius(10)
+                }.padding()
+                
+                NavigationLink(destination: GameView(AISelected: AISelected)) {
+                    Text("Start Game")
+                        .font(.title)
+                        .padding()
+                        .background(startColor)
+                        .foregroundColor(Color("textColor"))
+                        .cornerRadius(10)
+                }
+                .padding()
             }
             .padding()
         }
-        .padding()
     }
 }
 
